4d417536d530758e5af47a82d2fb65e371e76a89
paulaonu2003 <44978465+paulaonu2003@users.noreply.github.com>
2018-12-03 18:51:12 +0200

    Add files via upload

---
+++ biti1dfghjkl;'.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, counter=0, m;
+    cin>>n;
+    m=n;
+    if(n==0)
+    {
+        counter=1;
+        return o;
+    }
+    if(n>=0)
+    {
+        while(n>0)
+        {
+            n=n>>1;
+            counter++;
+        }
+    }
+    else
+        if(n==-1)
+    {
+        counter=1;
+        return 0;
+    }
+    else
+        while(n<-1)
+    {
+        n=n>>1;
+        counter++;
+    }
+    while(n>0)
+        {
+            n=n>>1;
+            counter++;
+        }
+        else
+            if(n==-1)
+    }
